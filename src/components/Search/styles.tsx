5e9f9dc5c9328456223f9e973899bf5a59792bc7
NesterenkoAndrew <79786120+nester7x@users.noreply.github.com>
2023-12-13 11:01:51 +0200

    fix: (fixed flashing during the request) (#2)
    * fix: (fixed flashing during the request)
    
    * refactor: (made loader for input)

--- src/components/Search/styles.tsx
+++ src/components/Search/styles.tsx
@@ -7,7 +7,7 @@ export const SearchInput = styled(Input)`
     width: 100%;
     font-size: 20px;
     line-height: 71%;
-    padding: 10px 15px;
+    padding: 10px 15px 10px 40px;
     height: min-content;
   }
 `;
